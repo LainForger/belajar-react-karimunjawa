cd4a309b18824909ab728cbda8250911000848d0
LainForger <lainforger@gmail.com>
2026-03-14 12:48:17 +0700

    Ubah tampilan utama App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        h1 > 🌊Karimunjawa Interaktif < /h1> <
+        p > Halo, saya < b > Lain Forger < /b>. Selamat datang di project React pertama saya!</p >
+        <
+        a className = "App-link"
+        href = "#"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Eksplorasi Sekarang <
+        /a> < /
+        header > <
+        /div>
+    );
 }
 
 export default App;
