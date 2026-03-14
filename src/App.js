9a74df4ff6cd43ebe339170d2eba38e7f046c315
LainForger <lainforger@gmail.com>
2026-03-14 14:07:51 +0700

    Deploy website Karimunjawa ke root

--- src/App.js
+++ src/App.js
@@ -9,28 +9,32 @@ function App() {
     "⛰️ Bukit Love Karimunjawa",
   ];
 
-return (
+  return (
     <div className="App">
       <header className="App-header">
         <h1>Karimunjawa</h1>
-        <p style={{ fontSize: '1.5rem', fontWeight: '300' }}>
+        <p style={{ fontSize: "1.5rem", fontWeight: "300" }}>
           Jelajahi Surga Tersembunyi di Jawa Tengah
         </p>
-        
+
         <div className="list-container">
-          <h3 style={{ borderBottom: '2px solid white', paddingBottom: '10px' }}>
+          <h3
+            style={{ borderBottom: "2px solid white", paddingBottom: "10px" }}
+          >
             Destinasi Populer
           </h3>
-          <ul style={{ listStyle: 'none', padding: 0, fontSize: '1.2rem' }}>
+          <ul style={{ listStyle: "none", padding: 0, fontSize: "1.2rem" }}>
             {destinasi.map((item, index) => (
-              <li key={index} style={{ margin: '10px 0' }}>
+              <li key={index} style={{ margin: "10px 0" }}>
                 {item}
               </li>
             ))}
           </ul>
         </div>
 
-        <p>Developed by <b>Lain Forger</b></p>
+        <p>
+          Developed by <b>Lain Forger</b>
+        </p>
 
         <a className="App-link" href="#" target="_blank">
           MULAI PETUALANGAN
