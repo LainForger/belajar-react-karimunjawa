e6236f38c912d0032912859945a2b0c79759d615
LainForger <lainforger@gmail.com>
2026-03-14 13:37:28 +0700

    Eksperimen warna gradasi background biar makin estetik

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import './App.css';
+import "./App.css";
+import gambarPantai from "./pantai.jpeg";
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        h1 > 🌊Karimunjawa Interaktif < /h1> <
-        p > Halo, saya < b > Lain Forger < /b>. Selamat datang di project React pertama saya!</p >
-        <
-        a className = "App-link"
-        href = "#"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Eksplorasi Sekarang <
-        /a> < /
-        header > <
-        /div>
-    );
+  const destinasi = [
+    "🏖️ Pantai Tanjung Gelam",
+    "🦈 Penangkaran Hiu Menjangan Besar",
+    "🤿 Spot Snorkeling Maer",
+    "⛰️ Bukit Love Karimunjawa",
+  ];
+
+return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Karimunjawa</h1>
+        <p style={{ fontSize: '1.5rem', fontWeight: '300' }}>
+          Jelajahi Surga Tersembunyi di Jawa Tengah
+        </p>
+        
+        <div className="list-container">
+          <h3 style={{ borderBottom: '2px solid white', paddingBottom: '10px' }}>
+            Destinasi Populer
+          </h3>
+          <ul style={{ listStyle: 'none', padding: 0, fontSize: '1.2rem' }}>
+            {destinasi.map((item, index) => (
+              <li key={index} style={{ margin: '10px 0' }}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <p>Developed by <b>Lain Forger</b></p>
+
+        <a className="App-link" href="#" target="_blank">
+          MULAI PETUALANGAN
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
